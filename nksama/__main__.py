71a14ddd9939f8d0f2a3cf2c04e3387c362a15de
Nine bish <88079604+ghoul-lover@users.noreply.github.com>
2021-12-09 21:16:42 +0530

    Update __main__.py

--- nksama/__main__.py
+++ nksama/__main__.py
@@ -11,3 +11,13 @@ if __name__ == "__main__":
     with bot:
         bot.send_message(-1001544622735 , "I'm Now online")
     
+def main():
+
+    if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
+        try:
+            dispatcher.bot.sendMessage(f"@mano_sanjiro_support", "[I am ready to fight](https://telegra.ph/file/a11036803a02158d4ab3d.mp4)", parse_mode=ParseMode.MARKDOWN) 
+        except Unauthorized:
+            LOGGER.warning(
+                "Bot isnt able to send message to support_chat, go and check!")
+        except BadRequest as e:
+            LOGGER.warning(e.message)
