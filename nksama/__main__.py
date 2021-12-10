c0787bb2f81b123362277d7e3bb421ab39a68507
Anshul Garg <93256357+AuraMoon55@users.noreply.github.com>
2021-12-10 10:54:55 +0530

    Update __main__.py

--- nksama/__main__.py
+++ nksama/__main__.py
@@ -9,15 +9,5 @@ if __name__ == "__main__":
     bot.run()
     musicbot.start()
     with bot:
-        bot.send_message(-1001544622735 , "I'm Now online")
+        bot.send_video(-1001747095926 , "https://telegra.ph/file/a11036803a02158d4ab3d.mp4", caption="I am ready to fight")
     
-def main():
-
-    if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
-        try:
-            dispatcher.bot.sendMessage(f"@mano_sanjiro_support", "[I am ready to fight](https://telegra.ph/file/a11036803a02158d4ab3d.mp4)", parse_mode=ParseMode.MARKDOWN) 
-        except Unauthorized:
-            LOGGER.warning(
-                "Bot isnt able to send message to support_chat, go and check!")
-        except BadRequest as e:
-            LOGGER.warning(e.message)
