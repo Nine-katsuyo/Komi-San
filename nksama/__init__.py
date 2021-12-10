b2cdc832caa7b2c81c486588349eaafa3e280ee9
Nine bish <88079604+ghoul-lover@users.noreply.github.com>
2021-12-10 20:16:36 +0530

    Update __init__.py

--- nksama/__init__.py
+++ nksama/__init__.py
@@ -5,6 +5,25 @@ from telegram.ext import Updater
 import os 
 import sys
 from nksama.mikeygban import mikeyClient
+
+OWNER_ID = config.settings.owner
+
+BOT_ID = config.telegram.bot.id
+
+BOT_NAME = config.telegram.bot.name
+
+BOT_USERNAME = config.telegram.bot.username
+
+LOG_CHANNEL = config.settings.log.chat_id
+
+SUDO_USERS = config.settings.sudo_users
+
+PREFIX = config.settings.commands.prefix
+
+BACKUP_CHAT = config.settings.backup.chat_id
+
+
+
 bot = Client(
     'bot',
     api_id=os.environ.get('API_ID'),
