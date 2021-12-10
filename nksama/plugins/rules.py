f4626ecb8c27a953661b088b269e261df02110eb
Nine bish <88079604+ghoul-lover@users.noreply.github.com>
2021-12-10 19:53:34 +0530

    Create rules.py

---
+++ nksama/plugins/rules.py
@@ -0,0 +1,53 @@
+from nksama import bot 
+
+from pyrogram import filters
+
+@bot.on_message(custom_filter.command(commands=('setrules')))
+
+def set_rules(client, message):
+
+    
+
+    chat_id = message.chat.id 
+
+    chat_title = message.chat.title
+
+    if not await isUserCan(message, permissions='can_change_info'):
+
+        return
+
+    
+
+    if not (
+
+        len(message.command) >= 2
+
+    ):
+
+        await message.reply(
+
+            "You need to give me rules to set!",
+
+            quote=True
+
+        )
+
+        return
+
+    
+
+    get_rules = message.text.markdown[len(message.command[0]) + 2 :]
+
+    set_rules_db(chat_id, get_rules)
+
+    await message.reply(
+
+        f"New rules for {html.escape(chat_title)} set successfully!",
+
+        quote=True
+
+    )
+
+
+  
+
